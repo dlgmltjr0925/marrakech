984bc4496d333610e83b45950bc280f0548c33b9
fine <hslee@insterior.co.kr>
2021-10-25 17:39:07 +0900

    feature: Express 적용

--- server.ts
+++ server.ts
@@ -2,17 +2,22 @@ import Socket from './libs/socket';
 import http from 'http';
 import next from 'next';
 import { parse } from 'url';
+import express from 'express';
 
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handler = app.getRequestHandler();
 
 app.prepare().then(() => {
-  const server = http.createServer((req, res) => {
+  const app = express();
+
+  app.all('*', async (req, res) => {
     const parsedUrl = parse(req.url || '', true);
 
-    handler(req, res, parsedUrl);
+    await handler(req, res, parsedUrl);
   });
+
+  const server = http.createServer(app);
 
   new Socket(server);
 
